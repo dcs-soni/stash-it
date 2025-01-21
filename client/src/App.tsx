252476609c7ca13189541510efdc44b4606b1912
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-01-22 00:03:08 +0530

    feat: add button component with tailwind styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-red-100">
+      <Button text="Stash It" variant="primary" startIcon={<PlusIcon />} />
+      <Button text="Share" variant="secondary" startIcon={<ShareIcon />} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { ReactElement } from "react";
+
+interface ButtonProps {
+  variant: "primary" | "secondary";
+  text: string;
+  startIcon: ReactElement;
+}
+
+const variantClasses = {
+  primary: "bg-blue-400 text-white-200",
+  secondary: "bg-blue-100 text-white-600",
+};
+
+const defaultStyles = "px-4 py-2 rounded-md flex items-center ";
+
+export function Button({ variant, text, startIcon }: ButtonProps) {
+  return (
+    <button className={`${variantClasses[variant] + " " + defaultStyles}`}>
+      <div className="pr-2">{startIcon}</div>
+      {text}
+    </button>
+  );
+}
