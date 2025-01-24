469494fac065e3a4d59ea84564830e392dea71f7
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-01-24 10:07:20 +0530

    refactor: move app logic to dashboard page and clean root(App) component'

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,51 @@
+import { Button } from "../components/Button";
+import { StashIcon } from "../icons/StashIcon";
+import { ShareIcon } from "../icons/ShareIcon";
+import { Card } from "../components/Card";
+import { CreateContentModal } from "../components/CreateContentModal";
+import { useState } from "react";
+import { Sidebar } from "../components/Sidebar";
+
+function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <div>
+      <Sidebar />
+      <div className="p-4 ml-60 min-h-screen bg-gray-100 ">
+        <CreateContentModal
+          open={modalOpen}
+          onClose={() => {
+            setModalOpen(false);
+          }}
+        />
+        <div className="flex justify-end gap-4 mb-8">
+          <Button
+            onClick={() => {
+              setModalOpen(true);
+            }}
+            text="Stash"
+            variant="primary"
+            startIcon={<StashIcon />}
+          />
+          <Button text="Share" variant="secondary" startIcon={<ShareIcon />} />
+        </div>
+        <div className="flex m-4 gap-4 font-semibold">
+          <Card
+            title="Trummp"
+            link="https://x.com/sfrei_/status/1881759013928591406"
+            type="twitter"
+          />
+
+          <Card
+            title="Youtube video"
+            link="https://www.youtube.com/watch?v=FFetMn8VXE4"
+            type="youtube"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
