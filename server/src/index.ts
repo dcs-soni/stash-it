52c58a6b079348913d6b7b47e21964971b6ce505
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-01-25 12:32:11 +0530

    feat: connect backend to frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,11 @@ import { z } from "zod";
 import bcrypt from "bcrypt";
 import { userMiddleware } from "./middleware";
 import { random } from "./utils";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post(
   "/api/v1/signup",
