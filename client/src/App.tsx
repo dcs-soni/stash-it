78e1bb1eb3c03a7919ce71f2a83b0633ab6948a6
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-01-25 08:48:23 +0530

    feat: add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import Dashboard from "./pages/dashboard";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
