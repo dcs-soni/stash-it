48869e57e2efb29075ff594c5ed53118efa23c57
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-24 13:56:46 +0530

    style: add tailwind configs and root css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsxts,tsx}",
+    "./app/**/*.{js,jsxts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        "slate-200": "#F4F4F5",
-      },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        brand: {
+          50: "#f0f7ff",
+          100: "#e0effe",
+          200: "#bae0fd",
+          300: "#7cc8fb",
+          400: "#36aaf4",
+          500: "#0c8de4",
+          600: "#006fc3",
+          700: "#015a9e",
+          800: "#064b82",
+          900: "#0a406d",
+        },
 
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
       fontFamily: {
         satisfy: ["Satisfy", "serif"],
         anta: ["Anta", "serif"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
